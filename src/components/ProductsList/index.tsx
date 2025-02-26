89f67a8fdaeb9af78c27450c075bcdb5cb174c01
Felipe Barbosa <mafebape140498@gmail.com>
2025-02-26 20:07:04 -0300

    terminando o footer

---
+++ src/components/ProductsList/index.tsx
@@ -0,0 +1,33 @@
+import Product from '../Product'
+import { Container, List } from './style'
+import ResidentEvil from '../../assets/images/resident.png'
+import Game from '../../models/Game'
+
+export type Props = {
+  title: string
+  background: 'gray' | 'black'
+  games: Game[]
+}
+
+const ProductsList = ({ title, background, games }: Props) => (
+  <Container background={background}>
+    <div className="container">
+      <h2>{title}</h2>
+      <List>
+        {games.map((game) => (
+          <Product
+            key={game.id}
+            category={game.category}
+            description={game.description}
+            image={game.image}
+            infos={game.infos}
+            system={game.system}
+            title={game.title}
+          />
+        ))}
+      </List>
+    </div>
+  </Container>
+)
+
+export default ProductsList
---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,104 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import Resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starwars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Residente Evil 4,conhecido no Japão como Biohazard 4, é um jogo survival...',
+    image: Resident,
+    infos: ['10%', 'R$ 250,00'],
+    system: 'Windows',
+    title: 'Residente Evil 4'
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Residente Evil 4,conhecido no Japão como Biohazard 4, é um jogo survival...',
+    image: Resident,
+    infos: ['5%', 'R$ 290,00'],
+    system: 'PS5',
+    title: 'Residente Evil 4'
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Residente Evil 4,conhecido no Japão como Biohazard 4, é um jogo survival...',
+    image: Resident,
+    infos: ['10%', 'R$ 250,00'],
+    system: 'Windows',
+    title: 'Residente Evil 4'
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Residente Evil 4,conhecido no Japão como Biohazard 4, é um jogo survival...',
+    image: Resident,
+    infos: ['10%', 'R$ 250,00'],
+    system: 'Windows',
+    title: 'Residente Evil 4'
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é  um RPG de ação em desenvolvimento pela Blizard Entertainment...',
+    title: 'Diablo IV',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description:
+      'Diablo IV é  um RPG de ação em desenvolvimento pela Blizard Entertainment...',
+    title: 'Zelda',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description:
+      'Diablo IV é  um RPG de ação em desenvolvimento pela Blizard Entertainment...',
+    title: 'Star Wars',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: starwars
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Diablo IV é  um RPG de ação em desenvolvimento pela Blizard Entertainment...',
+    title: 'Residente Evil 5',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: Resident
+  }
+]
+
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
